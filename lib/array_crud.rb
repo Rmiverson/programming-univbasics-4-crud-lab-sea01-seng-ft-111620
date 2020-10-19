6e7f0f4211c80975b6305df0ba6b0d882cb3ada9
Riley Iverson <rmiverson99@gmail.com>
2020-10-19 21:03:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array = arraycreate_an_array.push(arrays!)
+  new_array = array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
